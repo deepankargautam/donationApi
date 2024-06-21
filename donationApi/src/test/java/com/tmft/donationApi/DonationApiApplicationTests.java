6275daa185d95a6c047ebd5c74c1c4b1618e7524
deepankargautam <80680970+deepankargautam@users.noreply.github.com>
2024-06-21 15:39:47 +0530

    Add files via upload

---
+++ donationApi/src/test/java/com/tmft/donationApi/DonationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmft.donationApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
